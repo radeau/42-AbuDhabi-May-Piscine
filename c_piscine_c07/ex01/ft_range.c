b8163f7219e46551b4b52672e5c1245c8bc35f24
Kevin <59310402+radeau@users.noreply.github.com>
2021-06-09 12:34:11 +0400

    fixed the code

--- c_piscine_c07/ex01/ft_range.c
+++ c_piscine_c07/ex01/ft_range.c
@@ -18,9 +18,14 @@ int		*ft_range(int min, int max)
 	int *range;
 
 	i = 0;
-	if (max < min || !(range = (int *)malloc(sizeof(int) * (max - min) + 1)))
+	if (min >= max)
 		return (0);
-	while (min <= max)
-		range[i++] = min++;
+	range = (int *)malloc(sizeof(int) * (max - min));
+	while (min < max)
+	{
+		*(range + i) = min;
+		min++;
+		i++;
+	}
 	return (range);
 }
